c29a553d560ec2e7d53a6b9f89c399fe7d0ff363
Rafael Chicarellil <rafaelsep20@gmail.com>
2021-03-03 15:15:14 -0300

    Adicionando redirecionamento

--- frontend/src/components/DashboardEstabelecimentos/index.jsx
+++ frontend/src/components/DashboardEstabelecimentos/index.jsx
@@ -1,20 +1,26 @@
 import React, { useState, useEffect } from "react";
 import * as S from "./styles";
 import api from "../../services/api";
+import {Redirect} from 'react-router-dom';
 
-import Clientes from '../Clientes';
+import Clientes from "../Clientes";
 
 function DashboardEstabelecimentos() {
   const [estabelecimentos, setEstabelecimentos] = useState([]);
   const [estabelecimentoSelecionado, setEstabelecimentoSelecionado] = useState(0);
+  const [autorizado, setAutorizado] = useState(true);
 
   var token = localStorage.getItem("key");
   api.defaults.headers.common["Authorization"] = token;
 
   async function buscarRestaurante() {
-    await api.get("").then((response) => {
-      setEstabelecimentos([...response.data]);
-    });
+    try {
+      await api.get("").then((response) => {
+        setEstabelecimentos([...response.data]);
+      });
+    } catch {
+      setAutorizado(!autorizado);
+    }
   }
 
   useEffect(() => {
@@ -23,6 +29,9 @@ function DashboardEstabelecimentos() {
 
   return (
     <S.Container>
+      {!autorizado &&
+        <Redirect to="/login"/>
+      }
       <label>Selecione um restaurante: </label>
       <select
         name="restaurante"
@@ -31,7 +40,7 @@ function DashboardEstabelecimentos() {
           setEstabelecimentoSelecionado(e.target.value);
         }}
       >
-          <option value="0"></option>
+        <option value="0"></option>
         {estabelecimentos.map((estabelecimento) => (
           <option key={estabelecimento.id} value={estabelecimento.id}>
             {estabelecimento.nome}
@@ -39,9 +48,9 @@ function DashboardEstabelecimentos() {
         ))}
       </select>
 
-      {estabelecimentoSelecionado!==0 && 
-            <Clientes idRestaurante={estabelecimentoSelecionado}/>
-      }
+      {estabelecimentoSelecionado !== 0 && (
+        <Clientes idRestaurante={estabelecimentoSelecionado} />
+      )}
     </S.Container>
   );
 }
